5f81edd1040b83ac6f968ae39b0d5eb936fd6841
Mohammed Saad <mohammedsaad20161995@gmail.com>
2023-06-29 22:24:35 +0300

    add DetailsScreen

---
+++ app/src/main/java/com/example/tickets/screens/details/DetailsScreen.kt
@@ -0,0 +1,13 @@
+package com.example.tickets.screens.details
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+
+@Composable
+fun DetailsScreen(
+    navController: NavController
+) {
+    Text(text = "test movie details screen ")
+
+}
