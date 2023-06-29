24aa83789f2b0d34c2b387fdcae8ad8b6ee01db7
Mohammed Saad <mohammedsaad20161995@gmail.com>
2023-06-29 22:20:34 +0300

    add BookingScreen

---
+++ app/src/main/java/com/example/tickets/screens/booking/BookingScreen.kt
@@ -0,0 +1,11 @@
+package com.example.tickets.screens.booking
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+
+@Composable
+fun BookingScreen(navController: NavController){
+    Text(text = "booking screen")
+
+}
